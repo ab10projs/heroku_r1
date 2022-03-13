74fc0557216b6c3ac478be761d5f1609b601f248
Anupam <analyticsprojects10@gmail.com>
2022-03-13 12:42:00 +0530

    Procfile

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,8 @@ templates =os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-# SECRET_KEY = 'django-insecure-lw_(t$=s-$@)o@#1xg!v*8kddxdg2bzt(lk1_hr)462oeqmb(x'
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-lw_(t$=s-$@)o@#1xg!v*8kddxdg2bzt(lk1_hr)462oeqmb(x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
